81b8649db7dd94e798f98446d90457145011dd41
JorYin <134739719+JorYin@users.noreply.github.com>
2025-03-28 15:25:31 -0400

    Login Page cards and implementation

--- client/src/app/about/page.tsx
+++ client/src/app/about/page.tsx
@@ -1,67 +1,50 @@
 'use client'
-import React from 'react';
-import { Typography, Divider, Row, Col } from 'antd';
-import AboutMe from '../../component/aboutMe';
-
-// Destructure Title and Paragraph components from Typography
-const { Title, Paragraph } = Typography;
+import FirstNav from "@/component/firstNav";
+import FooterSection from "@/component/footer";
+import AboutMe from "@/component/aboutMe";
 
 // Define the About component
 const About = () => {
   return (
-    // Main container div with padding
-    <div className="site-layout-content" style={{ padding: '24px 0' }}>
-      {/* Title for the About section */}
-      <Title level={2} className="text-2xl font-bold">About Spark!Bytes</Title>
-      
-      {/* Paragraph describing the Spark Bytes application */}
-      <Paragraph>
-        Spark Bytes is a web application for Boston University (BU) to post events that
-        provide foods or snacks. The aim is to reduce food waste resulting from
-        over-purchasing for events and at the same time, help BU constituencies access
-        free food. Teams of 4-5 CS391 S1 students will develop a working web application
-        system that enables Spark!Bytes. That is Students or faculty at BU can use the app to see what places on campus have free food left over from events and they can then go and get free food. Thus, reducing food waste on campus and helping out both students and faculty get a free meal.
-      </Paragraph>
-      
-      {/* Divider line */}
-      <Divider />
-
-      {/* Title for the Meet the Team section */}
-      <Title level={2} className="text-2xl font-bold">Meet the Team</Title>
-      
-      {/* Row container with gutter spacing and centered content */}
-      <Row gutter={[16, 16]} justify="center">
-        {/* Column for each team member */}
-        <Col xs={24} sm={12} md={8}>
+    <main className="flex flex-col min-h-screen">
+      <FirstNav />
+      <section className="my-20 mx-4">
+        <div>
+          <h2 className="text-text-primary text-4xl lg:text-6xl font-montserrat font-bold mb-8 text-center">Meet the Team!</h2>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-8 justify-center items-center max-w-6xl mx-auto">
+          
           <AboutMe 
-            name="Member 1" 
-            bio="This is the bio of Member 1." 
-            imageUrl="https://via.placeholder.com/100" 
+            linkedIn="https://www.linkedin.com/in/jcwang27/"
+            name="Justin Wang" 
+            bio="BU community members browse and find events with available food" 
+            imageUrl="https://media.licdn.com/dms/image/v2/D4D03AQEhOhCgj4GV0w/profile-displayphoto-shrink_400_400/B4DZSay.eZGkAg-/0/1737763842528?e=1748476800&v=beta&t=0zLCLhJ3vun1IbuDwb7yfW1rUl1CVsMVhJ7tCsIbr14"
           />
-        </Col>
-        <Col xs={24} sm={12} md={8}>
+          
           <AboutMe 
-            name="Member 2" 
-            bio="This is the bio of Member 2." 
-            imageUrl="https://via.placeholder.com/100" 
+            linkedIn="https://www.linkedin.com/in/thaodanghoang/"
+            name="Thao Hoang" 
+            bio="BU community members browse and find events with available food" 
+            imageUrl="https://media.licdn.com/dms/image/v2/D4E03AQEpoI4OE6OXUA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1723077015556?e=1748476800&v=beta&t=8XxS_px1uzL4zrad7EYDgMP2cxlc0BO_rn1xAfgcEEk"
           />
-        </Col>
-        <Col xs={24} sm={12} md={8}>
+          
           <AboutMe 
-            name="Member 3" 
-            bio="This is the bio of Member 3." 
-            imageUrl="https://via.placeholder.com/100" 
+            linkedIn="https://www.linkedin.com/in/jiafei-fan-1a17b7335/"
+            name="Jiafei Fan" 
+            bio="BU community members browse and find events with available food" 
+            imageUrl="https://media.licdn.com/dms/image/v2/D4E03AQFQfTSH-lu2KQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1730228306674?e=1748476800&v=beta&t=dnIXD_HGa5vIYSdLLZYTegpU7zaTEuK5J8O4HBRs7dc"
           />
-        </Col>
-        <Col xs={24} sm={12} md={8}>
+          
           <AboutMe 
-            name="Member 4" 
-            bio="This is the bio of Member 4." 
-            imageUrl="https://via.placeholder.com/100" 
+            linkedIn="https://www.linkedin.com/in/j0rdanyin/"
+            name="Jordan Yin" 
+            bio="BU community members browse and find events with available food" 
+            imageUrl="https://media.licdn.com/dms/image/v2/D4E03AQGn_QD1IpNFpA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1714795021781?e=1748476800&v=beta&t=gRzGmOnQgtBGSerDARbHOqICjOWeNefN7NE9Sjd469Q"
           />
-        </Col>
-      </Row>
-    </div>
+        </div>
+      </section>
+      <FooterSection />
+    </main>
   );
 };
 
--- client/src/component/aboutMe.tsx
+++ client/src/component/aboutMe.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import { Card, Avatar, Typography } from 'antd';
-
-const { Text, Paragraph } = Typography;
-
 // Define an interface to specify the types of props that the AboutMe component will accept
 interface AboutMeProps {
   name: string;        // The name of the group member
   bio: string;         // The bio of the group member
   imageUrl: string;    // The URL of the group member's image
+  linkedIn: string;
 }
 
 // Define a functional React component named AboutMe that accepts props of type AboutMeProps
-const AboutMe: React.FC<AboutMeProps> = ({ name, bio, imageUrl }) => {
+const AboutMe: React.FC<AboutMeProps> = ({ name, bio, imageUrl, linkedIn }) => {
   return (
-    <Card style={{ width: 300, textAlign: 'center', margin: '20px auto' }}>
-      {/* Avatar component to display the group member's image */}
-      <Avatar src={imageUrl} size={100} style={{ marginBottom: '20px' }} />
-      {/* Text component to display the group member's name */}
-      <Text strong>{name}</Text>
-      {/* Paragraph component to display the group member's bio */}
-      <Paragraph>{bio}</Paragraph>
-    </Card>
+    <a target="_blank" href={linkedIn}>
+      <div className="border-2 border-text-primary rounded-lg px-4 py-8 shadow-lg">
+        {/** Container for the actual image and text */}
+        <div className="flex flex-col text-center">
+          <img 
+            src={imageUrl}
+            alt={`${name}'s profile`}
+            className="w-40 h-40 rounded-full object-cover mx-auto mb-4"
+          />
+          {/** Container for the text and also has a text box so that if there is overflow the cards won't be so different in height */}
+          <div className="text-text-primary ">
+          <h2 className="text-2xl lg:text-3xl font-montserrat font-bold my-6">{name}</h2>
+            <div className="font-inter text-text-primary text-sm lg:text-base max-w-xs mx-auto overflow-y-auto"
+              style={{ maxHeight: '100px' }} // Or adjust based on how much space you want
+            >
+              {bio}
+            </div>
+          </div>
+        </div>
+      </div>
+    </a>
   );
 };
 
