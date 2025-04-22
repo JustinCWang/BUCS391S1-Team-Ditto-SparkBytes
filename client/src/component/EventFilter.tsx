f629d5d1d894bfb50d7b1d0cded9036198ce4c55
JorYin <134739719+JorYin@users.noreply.github.com>
2025-04-22 11:42:56 -0400

    Change Z index on pop ups so that the nav doesn't go over it

--- client/src/component/EventFilter.tsx
+++ client/src/component/EventFilter.tsx
@@ -99,7 +99,7 @@ const EventFilter: React.FC<FilterProps> = ({ isOpen, onClose, onApply }) => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          className="fixed inset-0 bg-black/50 flex items-center justify-center z-50"
+          className="fixed inset-0 bg-black/50 flex items-center justify-center z-[999]"
         >
           <motion.div
             initial={{ y: 40, opacity: 0 }}
