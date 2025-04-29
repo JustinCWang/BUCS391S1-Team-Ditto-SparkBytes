4729a64fdca26ad5c658be6b82d65a67685cb37d
Justin Wang <jwlion14@gmail.com>
2025-04-29 17:48:24 -0400

    Updated about page

--- client/src/app/about/page.tsx
+++ client/src/app/about/page.tsx
@@ -17,29 +17,29 @@ const About = () => {
           <AboutMe 
             linkedIn="https://www.linkedin.com/in/jcwang27/"
             name="Justin Wang" 
-            bio="BU community members browse and find events with available food" 
+            bio="Scrum Master and Backend Developer" 
             imageUrl="https://media.licdn.com/dms/image/v2/D4D03AQEhOhCgj4GV0w/profile-displayphoto-shrink_400_400/B4DZSay.eZGkAg-/0/1737763842528?e=1748476800&v=beta&t=0zLCLhJ3vun1IbuDwb7yfW1rUl1CVsMVhJ7tCsIbr14"
           />
           
           <AboutMe 
             linkedIn="https://www.linkedin.com/in/thaodanghoang/"
             name="Thao Hoang" 
-            bio="BU community members browse and find events with available food" 
+            bio="Designer and Frontend Developer" 
             imageUrl="https://media.licdn.com/dms/image/v2/D4E03AQEpoI4OE6OXUA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1723077015556?e=1748476800&v=beta&t=8XxS_px1uzL4zrad7EYDgMP2cxlc0BO_rn1xAfgcEEk"
           />
           
           <AboutMe 
             linkedIn="https://www.linkedin.com/in/jiafei-fan-1a17b7335/"
             name="Jiafei Fan" 
-            bio="BU community members browse and find events with available food" 
+            bio="Fullstack Developer" 
             imageUrl="https://media.licdn.com/dms/image/v2/D4E03AQFQfTSH-lu2KQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1730228306674?e=1748476800&v=beta&t=dnIXD_HGa5vIYSdLLZYTegpU7zaTEuK5J8O4HBRs7dc"
           />
           
           <AboutMe 
             linkedIn="https://www.linkedin.com/in/j0rdanyin/"
             name="Jordan Yin" 
-            bio="BU community members browse and find events with available food" 
-            imageUrl="https://media.licdn.com/dms/image/v2/D4E03AQGn_QD1IpNFpA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1714795021781?e=1748476800&v=beta&t=gRzGmOnQgtBGSerDARbHOqICjOWeNefN7NE9Sjd469Q"
+            bio="Design head and Fullstack Developer" 
+            imageUrl="https://media.licdn.com/dms/image/v2/D4E03AQHOGHBymXR-ow/profile-displayphoto-shrink_800_800/B4EZZaVbQPHkAc-/0/1745272288662?e=1751500800&v=beta&t=eE9pkOkucy6NoDx_eMZuqFOaSWxTMNF-IA_RFLWwe9o"
           />
         </div>
       </section>
